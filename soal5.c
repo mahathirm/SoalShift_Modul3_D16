c3876339c7a675c7003bcac650652157a42928f7
Unknown <mahathirmi@gmail.com>
2017-10-24 20:16:15 +0700

    nyicil dulu pal

---
+++ soal5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h> 
+
+void *print_message_function( void *ptr );
+{ 
+
+}
+int main()
+{
+        pthread_t thread [10];
+
+
+
+        exit(EXIT_SUCESS);
+}
+
