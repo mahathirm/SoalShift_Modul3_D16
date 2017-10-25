c036f73f4d600903103dfc8dcba198ea5f2aa188
Unknown <naufal.andira@gmail.com>
2017-10-25 14:22:31 +0700

    mabok thir

--- soal5.c
+++ soal5.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <pthread.h> 
 
 void *print_message_function( void *ptr );
 { 
+	FILE *bukanovel;
+	bukanovel = fopen("/home/naufal/Desktop/novel.txt", "r");
 
 }
 int main()
