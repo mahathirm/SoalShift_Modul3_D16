fac23165f567940a3157fed070cd7efd239b9213
naufalap <32903336+naufalap@users.noreply.github.com>
2017-11-02 16:52:39 +0700

    Update Soal4.c

--- Soal4.c
+++ Soal4.c
@@ -3,17 +3,34 @@
 #include <stdlib.h>
 #include <unitsd.h>
 
-void *faktorial(){
+void *faktorial(void *faktor){
 	int angka, hitung, hasil = 1;
-	scanf("%d", &angka);
+	
+	angka =(int)faktor
 	if ( angka <= 0)
 		printf("Angka tidak bisa negatif");
 	else
-		for (hitung = angka; hitung >= 1; hitung--) {
+		for (hitung = 1; hitung >= angka; hitung++) {
 		hasil = hasil * hitung;
 	}
 	printf("Hasil %d! = %d\n", angka, hasil);
 	return 0;
 }
 
-int main(){
+int main()
+{
+	pthread_t nopal[20];
+	int a,i,j,k;
+	char spasi;
+	int athir[20];
+	while(1)
+	{
+		scanf("%d%c",&athir[i],&spasi);
+		if (spasi == '\n')
+		{
+			break;	
+		}
+		i++;
+	}
+	
+}
